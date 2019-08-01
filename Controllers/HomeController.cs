a1ee02e75f06f9d117791504f81919acaa9fa3f2
Xinran <52697166+xrma99@users.noreply.github.com>
2019-08-01 17:23:22 +0800

    2019.8.1
    Try to upload file and store it in the ~/dllfiles.
    Unluckily I failed...
    But I believe I can make it tomorrow!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FileUpload.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Fileexecute(IFormFile file)
+        {
+            if (file != null)
+            {
+                string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string SavePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", filename);
+                using(var stream=new FileStream(SavePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
