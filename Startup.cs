356392c5df5e93f634db5aceb32e4b39f5375ed6
Xinran <52697166+xrma99@users.noreply.github.com>
2019-08-05 09:54:08 +0800

    2019.8.2

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using FileUpload.Models;
 
 namespace FileUpload
 {
@@ -33,6 +35,9 @@ namespace FileUpload
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<FileUploadContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("FileUploadContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
